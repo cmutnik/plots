14c5405e9a3bc78db7303168de5973378ccf0200
Corey Mutnik <cmutnik@hawaii.edu>
2020-07-07 17:36:38 -0400

    xkcd style, example

--- Python/README.md
+++ Python/README.md
@@ -1,7 +1,19 @@
 # Plotting in Python
 
+----
 ### Line Plot
 
 This figure was made using [LinePlot.py](LinePlot/LinePlot.py).
 
 ![Foo](LinePlot/lp.png)
+
+----
+### Styles
+
+There are many different plotting styles in python, from `seaborn` (professional), to `xkcd` (fun).<br>
+The script [`xkcd_style.py`](./xkcd_style.py) shows how to plot using the xkcd style.  Setting this style, with the line:
+```py
+plt.xkcd()
+```
+allows you to make plots like the one shown here:
+![](./figs/XKCD_plot.png)
---
+++ Python/xkcd_style.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# plot using the xkcd style
+plt.xkcd()
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+plt.xticks([])
+plt.yticks([])
+ax.set_ylim([-30, 10])
+
+data = np.ones(100)
+data[70:] -= np.arange(30)
+
+plt.annotate(
+    'THE DAY I\nMOVED OUT OF\nMY PARENTS HOUSE',
+    xy=(70, 1), arrowprops=dict(arrowstyle='->'), xytext=(15, -10))
+
+plt.plot(data)
+
+plt.xlabel('time')
+plt.ylabel('my overall health')
+
+plt.savefig('figs/XKCD_plot.png')
